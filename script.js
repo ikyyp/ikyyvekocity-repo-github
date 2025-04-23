5a1f2994b79a6f5f32c473fe15e36d51555ac6d5
ikyyp <kyynokos@gmail.com>
2025-04-23 22:16:07 +0700

    fucejs

---
+++ script.js
@@ -0,0 +1,6 @@
+// Efek interaktif tombol
+const button = document.querySelector('.interaktif-btn');
+
+button.addEventListener('click', function() {
+    alert("Tombol Interaktif telah ditekan!");
+});
